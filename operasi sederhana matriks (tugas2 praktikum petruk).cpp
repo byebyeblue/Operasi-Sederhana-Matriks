6709e6be9cd9cc0ab96780ec7a2dbca16b75b367
nurayu1039 <62587005+nurayu1039@users.noreply.github.com>
2020-03-27 20:53:42 +0700

    Update operasi sederhana matriks (tugas2 praktikum petruk).cpp

--- operasi sederhana matriks (tugas2 praktikum petruk).cpp
+++ operasi sederhana matriks (tugas2 praktikum petruk).cpp
@@ -6,9 +6,9 @@ using namespace std;
     Tugas Pengganti Pertemuan 4
     Implementasi Pointer Array dan Pointer Fungsi Menggunakan C++
     Anggota:
-    Nur Ayu Octarina (1917051039) - uname : nurayu1039
-    Fanirizki Sofiyana (1917051050) - uname : byebyeblue
-    Ale Habsyi Arwendi (1917051067) - unanme : mobby14
+    Nur Ayu Octarina (1917051039) - nurayu1039
+    Fanirizki Sofiyana (1917051050) - byebyeblue
+    Ale Habsyi Arwendi (1917051067) - mobby14
   */
 
 int i, j, k, m, n, p, q, jumlah = 0;
